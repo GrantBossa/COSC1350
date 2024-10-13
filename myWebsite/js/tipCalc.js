8b5515feee935c63de48653f12925cd0c730a5df
Grant A. Bossa <Grantbossa@westernwyoming.edu>
2024-10-12 21:31:07 -0600

    Updated Hdr info and Calculator functions
    made header info consistent
    Updated and completed Tip Calculator for Mid-Term
    corrected some inline compatibility issues in contact.html regarding WWCC map information

--- myWebsite/js/tipCalc.js
+++ myWebsite/js/tipCalc.js
@@ -1,18 +1,9 @@
 //
 //	Student Name:  Grant Bossa
 //	File Name: tipCalc.js
-//	Date: Oct 12, 2024
+//	Date Created: Oct 12, 2024
+//	Last Modified: October 12, 2024
 //
- function validateMyForm() {
-    ;
-
-    if ( document.getElementById("billAmount").value <= 0 || null ){
-        document.getElementById("btnCalcTip").preventDefault(); // Prevent form submission
-        return false;
-    } else {
-        return true;    
-    }
- }
  function calculateTip() {
     // CONSTANTS
     TEN_PERCENT = .10
@@ -21,26 +12,20 @@
     TWENTYFIVE_PERCENT=.25
     
     // VARIABLES
-    var billAmount=0
-    var tipPercentage=0
-    var tipPercent = 0
-    let tipAmount = 0
-    let totalAmount=0
-
-    
+    var billAmount
+    var tipPercentage
+    var tipPercent
+    let tipAmount
+    let totalAmount
+   
     // set variable values from form
     billAmount = document.getElementById("billAmount").value;
     tipPercentage = document.getElementById("tipPercentage").value    
     
     // test billAmount to be greater than zero, else msg'Enter Bill Amount'
        
-     if ( billAmount <= 0 || null ){
+    if ( billAmount <= 0 || null ){
         alert("Please enter a Bill Amount")
-        document.getElementById("btnCalcTip").preventDefault(); // Prevent form submission
-        // reset total amounts to zero
-        document.getElementById("tipAmount").innerHTML= 0;
-        document.getElementById("totalAmount").innerHTML= 0;
- 
     } else {
         // calc tip amount based on selected value 
         if (tipPercentage.includes("25%")) {
@@ -57,10 +42,11 @@
         // calc total amount
         totalAmount=(billAmount*1)+(tipAmount*1)
 
-        // set form values from  variables
-        document.getElementById("tipAmount").innerHTML= Math.trunc((tipAmount+.005)*100)/100;
-        document.getElementById("totalAmount").innerHTML= Math.trunc((totalAmount+.005)*100)/100;
+        // set form values from  variables 
+        document.getElementById("tipAmount").innerHTML= tipAmount.toFixed(2);
+        document.getElementById("totalAmount").innerHTML= totalAmount.toFixed(2);
         
-         alert ("billAmount = " + billAmount +"\ntipPercentage = " + tipPercentage+"\ntipAmount = " + document.getElementById("tipAmount").innerHTML + "\ntotalAmount = " + document.getElementById("totalAmount").innerHTML)
+        // for debugging, uncomment alert below
+        // alert ("billAmount = " + billAmount +"\ntipPercentage = " + tipPercentage+"\ntipAmount = " + document.getElementById("tipAmount").innerHTML + "\ntotalAmount = " + document.getElementById("totalAmount").innerHTML)
     }
 }
